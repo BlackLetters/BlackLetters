4bbec00b243b8d573a2ca5b7d7f1679980dadc52
CristiPaul <32931823+CristiPaul@users.noreply.github.com>
2020-04-10 09:34:04 +0300

    Add files via upload

---
+++ Context/BibliotecaContext.cs
@@ -0,0 +1,26 @@
+﻿using Biblioteca.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Biblioteca.Context
+{
+    public class BibliotecaContext: DbContext
+    {
+        public BibliotecaContext(DbContextOptions<BibliotecaContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<RegistredUserSub> RegistredUserSubs { get; set; }
+        public DbSet<RegistredUser> RegistredUsers { get; set; }
+        public DbSet<UnregistredUser> UnregistredUsers { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+
+
+    }
+}
