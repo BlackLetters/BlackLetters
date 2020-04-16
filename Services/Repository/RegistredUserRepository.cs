b3ca207f42ec671661bcca918befc9a2b005371c
Florea Andrei Cristian <32931850+LokiDescendent@users.noreply.github.com>
2020-04-17 00:37:07 +0300

    Add files via upload

---
+++ Services/Repository/RegistredUserRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Biblioteca.Context;
+using Biblioteca.Models;
+using Biblioteca.Services.Interfaces;
+
+
+namespace Biblioteca.Services.Repository
+{
+    public class RegistredUserRepository : RepositoryBase<RegistredUserRepository>, IRegistredUserRepository
+    {
+        public RegistredUserRepository(BibliotecaContext repositoryContext)
+            : base(repositoryContext)
+        {
+
+        }
+
+        public void Create(RegistredUser entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(RegistredUser entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Exists(int id)
+        {
+            var found = RepositoryContext.RegistredUsers.Any(e => e.ID == id);
+            return found;
+        }
+
+        public RegistredUser FindByCondition(Expression<Func<RegistredUser, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(RegistredUser entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        List<RegistredUser> IRepositoryBase<RegistredUser>.FindAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
