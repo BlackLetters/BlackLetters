c0241ab546a5a124e54c78266d49de21f430517c
Vladcraioveanu <44982126+Vladcraioveanu@users.noreply.github.com>
2020-04-16 23:24:22 +0300

    Add files via upload

---
+++ Services/Interfaces/IRegistredUserSubRepository.cs
@@ -0,0 +1,13 @@
+﻿using Biblioteca.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Biblioteca.Services.Interfaces
+{
+    interface IRegistredUserSubRepository: IRepositoryBase <RegistredUserSub>
+    {
+        public bool Exists (int ID);
+    }
+}
