5b8434bfba71c23cc66231eff9022d35f5f6bfa7
Florea Andrei Cristian <32931850+LokiDescendent@users.noreply.github.com>
2020-04-17 00:36:33 +0300

    Add files via upload

---
+++ Services/Interfaces/IRegistredUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Biblioteca.Models;
+
+namespace Biblioteca.Services.Interfaces
+{
+    public interface IRegistredUserRepository : IRepositoryBase<RegistredUser>
+    {
+        public bool Exists(int id);
+    }
+}
