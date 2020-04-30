ea268964b5ad55613ef3d6367ef68a0ddc3ce1c3
SorinMihai <33117840+SorinMihai@users.noreply.github.com>
2020-04-30 20:59:55 +0300

    Add files via upload

---
+++ BookRepository.cs
@@ -0,0 +1,27 @@
+﻿using Biblioteca.Context;
+using Biblioteca.Models;
+using Biblioteca.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Biblioteca.Services.Repository
+{
+    public class BookRepository : RepositoryBase<Book>, IBookRepository
+    {
+        public BookRepository(BibliotecaContext repositoryContext)
+            : base(repositoryContext)
+        {
+
+        }
+
+        public bool Exists(int id)
+        {
+            var found = RepositoryContext.Books.Any(e => e.ID == id);
+            return found;
+        }
+
+
+    }
+}
---
+++ BooksController.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Biblioteca.Context;
+using Biblioteca.Models;
+using Biblioteca.Services.Interfaces;
+
+namespace Biblioteca.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly BibliotecaContext _context;
+        private IRepositoryWrapper repo;
+
+        public BooksController(IRepositoryWrapper repo)
+        {
+            repo = repo;
+        }
+
+        // GET: Books
+        public async Task<IActionResult> Index()
+        {
+            var book = repo.Book.FindAll();
+            return View(book);
+        }
+
+        // GET: Books/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book = repo.Book.FindByCondition(t => t.ID == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return View(book);
+        }
+
+        // GET: Books/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Books/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Title,Price,Status,Type,Author")] Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                repo.Book.Create(book);
+                repo.Save();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(book);
+        }
+
+        // GET: Books/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book = repo.Book.FindByCondition(t => t.ID == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return View(book);
+        }
+
+        // POST: Books/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Title,Price,Status,Type,Author")] Book book)
+        {
+            if (id != book.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    repo.Book.Update(book);
+                    repo.Save();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BookExists(book.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(book);
+        }
+
+        // GET: Books/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book = repo.Book.FindByCondition(t => t.ID == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return View(book);
+        }
+
+        // POST: Books/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var book = repo.Book.FindByCondition(t => t.ID == id);
+            repo.Book.Delete(book);
+            repo.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BookExists(int id)
+        {
+            bool found = repo.Book.Exists(id);
+            return found;
+        }
+    }
+}
